a161eb6cca8b7d09f66b70c759ebb9ef24ca50e9
Spamakin <anakin@icalyx.com>
2021-07-03 16:30:18 -0500

    basics of message edits

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
+# TODO: 
+#       Pass query and result and stuff to reaction event
+#       Make sure only person who sent command can edit message
+
 from discord.ext import commands
-import urllib, json
+import urllib, json, discord
 
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name="printSequence")
 async def printing(ctx, query: str):
-    search = "https://oeis.org/search?fmt=json&q=" + query + "&start=0"
+
+    search = "https://oeis.org/search?fmt=json&q=" + query + "&start=" + str(0)
     with urllib.request.urlopen(search) as url:
         data = json.loads(url.read().decode())
         if int(data["count"]) > 0 and data["results"] == None:
@@ -16,11 +21,26 @@ async def printing(ctx, query: str):
             await ctx.send("No results found.")
             return
         
-        currentCount = int(data["count"])
         firstResult = data["results"][0]
-        message = await ctx.send(firstResult["name"])
+        message = await ctx.send(firstResult["name"] + '\n' + 'Result: 0' + '\n' + 'Query: ' + query)
         await message.add_reaction('◀️')
         await message.add_reaction('▶️')
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    if user.bot:
+        print("bot self reacted")
+    else:
+        msg = reaction.message.content
+        contentArray = msg.splitlines()
+        result = contentArray[1][8:]
+        newResult = int(result) + 1
+        query = contentArray[2][7:]
+        newSearch = "https://oeis.org/search?fmt=json&q=" + query + "&start=" + str(newResult)
+        with urllib.request.urlopen(newSearch) as url:
+            data = json.loads(url.read().decode())
+            searchResult = data["results"][0]
+            await reaction.message.edit(content=searchResult["name"] + '\n' + 'Result: ' + str(newResult) + '\n' + 'Query: ' + query)
 
 with open("BOT_TOKEN.txt", "r") as token_file:
     TOKEN = token_file.read()
